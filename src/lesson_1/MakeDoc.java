7f0941471ed96e26a701f617b76e5ed26422f626
khaledbakeer <bakeer@khaled.de>
2019-04-17 11:49:46 +0200

    Lesson 1

--- src/lesson_1/MakeDoc.java
+++ src/lesson_1/MakeDoc.java
@@ -3,6 +3,6 @@ package lesson_1;
 /**
  * https://www.jetbrains.com/help/idea/working-with-code-documentation.html
  */
-public class comments {
+public class MakeDoc {
 // 2 shift -> fix doc document
 }
