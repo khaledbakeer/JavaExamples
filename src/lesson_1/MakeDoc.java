b154392390dd18445006fb555dd5ece6b8977106
Khaled <bakeer@khaled.de>
2019-04-23 20:20:54 +0200

    Update MakeDoc.java

--- src/lesson_1/MakeDoc.java
+++ src/lesson_1/MakeDoc.java
@@ -1,6 +1,7 @@
 package lesson_1;
 
 /**
+ * In IntelliJ click two times Shift then write "fix doc document", more information see here
  * https://www.jetbrains.com/help/idea/working-with-code-documentation.html
  */
 public class MakeDoc {
