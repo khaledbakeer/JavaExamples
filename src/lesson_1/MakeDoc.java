ee1ba9d9bba241fb0a33986d87a0a1e617af3bd3
Khaled <bakeer@khaled.de>
2019-04-23 20:23:13 +0200

    Update MakeDoc.java

--- src/lesson_1/MakeDoc.java
+++ src/lesson_1/MakeDoc.java
@@ -1,9 +1,16 @@
 package lesson_1;
 
 /**
+ * Java doc:
+ * ---------
  * In IntelliJ click two times Shift then write "fix doc document", more information see here
  * https://www.jetbrains.com/help/idea/working-with-code-documentation.html
  */
 public class MakeDoc {
-// 2 shift -> fix doc document
+  // single line comment.
+  // another single line comment
+  /*
+   * This ist a block comment
+   *
+   */
 }
